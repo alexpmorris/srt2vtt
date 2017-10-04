8741e292c9d8e234103db146851e5d3b9412185a
alexpmorris <qd01a@yourika.com>
2017-10-04 15:26:54 -0400

    first commit

---
+++ srt2vtt_jar/20170930/baw/baw.bat
@@ -0,0 +1,1 @@
+java -jar ..\..\srt2vtt.jar @officialfuzzy.srt *.srt baw-20170930.vtt baw-20170930.htm
---
+++ srt2vtt_jar/20170930/gsg/gsg.bat
@@ -0,0 +1,1 @@
+java -jar ..\..\srt2vtt.jar @officialfuzzy.srt *.srt gsg-20170930.vtt gsg-20170930.htm
